9c8f08c83cfa4f57e54f2da2a2f7659028c32e7d
Dominik Schneider <dominik.schneider@colorado.edu>
2016-09-21 19:50:29 -0600

    new resolution vegheight

--- scripts/preprocess_veg.R
+++ scripts/preprocess_veg.R
@@ -1,5 +1,9 @@
 library(raster)
 
+elev_rast=raster('data/elevation/tuo_dem50m.tif')
+
 vegheight_rast=raster('data/veg/raw/tuo_vegheight_3m.tif')
-aggregate(vegheight_rast,fact=167,method='median',filename='data/veg/tuo_vegheight_501m.tif',NAflag=-99,overwrite=T)
-resample(raster('data/veg/tuo_vegheight_501m.tif'), elev_rast,method='ngb',filename='data/veg/tuo_vegheight_500m.tif',NAflag=-99,overwrite=T)
+aggregate(vegheight_rast,fact=17,method='median',filename='data/veg/tuo_vegheight_51m.tif',NAflag=-99,overwrite=T)
+resample(raster('data/veg/tuo_vegheight_51m.tif'), elev_rast,method='ngb',filename='data/veg/tuo_vegheight_50m.tif',NAflag=-99,overwrite=T)
+
+
