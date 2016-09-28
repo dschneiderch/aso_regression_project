a373e0e0a022978e4a36bbcc80f1bbef073a2e90
Dominik Schneider <dominik.schneider@colorado.edu>
2016-09-27 19:19:48 -0600

    moved r2 and pctmae calcs to run_splitsample from stats_splitsample

--- scripts/run_splitsample-modeling.R
+++ scripts/run_splitsample-modeling.R
@@ -109,7 +109,7 @@ saveRDS(allphvmdls %>% select(-contains('aug'),-contains('obj')), paste0(pathout
 
 allmdldata <- allphvmdls %>%
   select(dte,.id,train,test)  # %>% filter(dte==unique(dte)[1:2])
-#rm(allphvmdls)
+# rm(allphvmdls)
 
 allasomdls <-
   allmdldata %>%
@@ -149,14 +149,16 @@ for(iyr in unique(allasomdls$yr)){
 
 allasomdls <- allasomdls %>% dplyr::select(dte,yr,.id,asodte,contains('aug'))
 
-if(!exists('allasomdls')){
+# phvaso models error statistics
+## calculate rmse and pct rmse to determine best aso date for each date simulated ----
+if(exists('allasomdls')){#'if/else' mostly here in case r crashes after saving
+  phvasomdls=allasomdls %>% dplyr::select(-phvasofsca_aug_glmmdl)
+} else {
   fns=dir('output/splitsample-modeling',pattern='phvasomdls_aug',full.names=TRUE)
   phvasomdls=bind_rows(lapply(fns,readRDS))
-} else {
-  phvasomdls=allasomdls %>% dplyr::select(-phvasofsca_aug_glmmdl)
 }
 
-stats=
+stats <-
   phvasomdls %>%
   # filter(dte==asodte) %>%
   group_by(dte,asodte,yr) %>%
@@ -177,16 +179,30 @@ stats %>%
             bestpctrmse=pctrmse[which.min(rmse)]) %>%
   readr::write_tsv(.,path=paste0(pathout,'bestasodates_phvaso_',ires,'.txt'))
 
-if(!exists('allasomdls')){
-  rm(phvasomdls)
+## calculate r2 and percent mae ----
+# fns <- dir(pathin,glob2rx(paste0('^phvasomdls_aug*',ires,'*.rds')),full.names=T)
+phvaso <-
+  # bind_rows(
+  #   lapply(fns,readRDS)
+  # ) %>%
+  phvasomdls %>%
+  rename(mdldte=dte) %>%
+  extractAug(.,'phvaso','glmmdl') %>%
+  group_by(mdldte,asodte,mdlpreds) %>%
+  calc_error()
+rm(phvasomdls)
+
+
+# phvasofsca models error statistics
+## calculate rmse and pct rmse to determine best aso date for each date simulated ----
+if(exists('allasomdls')){
+  phvasofscamdls=allasomdls %>% dplyr::select(-phvaso_aug_glmmdl)
+} else {
   fns=dir('output/splitsample-modeling',pattern='phvasofscamdls_aug',full.names=TRUE)
   phvasofscamdls=bind_rows(lapply(fns,readRDS))
-} else {
-  phvasofscamdls=allasomdls %>% dplyr::select(-phvaso_aug_glmmdl)
 }
 
-
-stats=
+stats <-
   phvasofscamdls %>%
   # filter(dte!=asodte) %>%
   group_by(dte,asodte,yr) %>%
@@ -205,5 +221,39 @@ stats %>%
             bestpctrmse=pctrmse[which.min(rmse)]) %>%
   readr::write_tsv(.,path=paste0(pathout,'bestasodates_phvasofsca_',ires,'.txt'))
 
+## calculate r2 and percent mae ----
+# fns  <- dir(pathin,glob2rx(paste0('^phvasofscamdls_aug*',ires,'*.rds')),full.names=T)
+phvasofsca <-
+  # bind_rows(
+  #   lapply(fns,readRDS)
+  # ) %>%
+  phvasofscamdls %>%
+  rename(mdldte=dte) %>%
+  extractAug(.,'phvasofsca','glmmdl')%>%
+  group_by(mdldte,asodte,mdlpreds) %>%
+  calc_error()
+rm(phvasofscamdls)
+
+
+## calculate r2 and percent mae ----
+phv <-
+  # readRDS(paste0(pathin,'phvmdls_augment_',ires,'.rds')) %>%
+  allphvmdls %>%
+  rename(mdldte=dte) %>%
+  extractAug(.,'phv','glmmdl') %>%
+  group_by(mdldte,mdlpreds) %>%
+  calc_error()
+
+phvfsca <-
+  # readRDS(paste0(pathin,'phvmdls_augment_',ires,'.rds')) %>%
+  allphvmdls %>%
+  rename(mdldte=dte) %>%
+  extractAug(.,'phvfsca','glmmdl') %>%
+  group_by(mdldte,mdlpreds) %>%
+  calc_error()
+
+# combine r2 and pct mae
+predictions=bind_rows(phv,phvfsca,phvaso,phvasofsca)
+saveRDS(predictions,paste0('output/splitsample-modeling/stats_combined_splitsample',ires,'.rds'))
 
 # }    # <------ uncomment to run sensitivity
